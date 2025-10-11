2118518c3bc28c95a6e03efe7de4d54d76244581
Ruben Garcia <rubendanielgarciat@gmail.com>
2025-10-10 23:04:04 -0400

    added authentication method like in Dev Connect API

---
+++ API/Extensions/AppUserExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+using API.DTOs;
+using API.Entities;
+using API.Interface;
+
+namespace API.Extensions;
+
+public static class AppUserExtensions
+{
+    public static UserDto ToDto(this AppUser user, ITokenService token)
+    {
+        return new UserDto
+        {
+            Id = user.Id,
+            Email = user.Email,
+            DisplayName = user.DisplayName,
+            Token = token.CreateToken(user)
+        };
+    }
+}
