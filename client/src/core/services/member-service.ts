a8fb65d8bd613eea22481e058ddafa48c5b22315
Ruben Garcia <rubendanielgarciat@gmail.com>
2025-10-14 13:59:29 -0400

    Themes + environment + daisy ui and more css from youtube/reddit/stackoverflow/ ollama. async pipe added and interceptor for token like in dev connect and route resolver (minimiza bloat)

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -1,8 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { AccountService } from './account-service';
-import { Member } from '../../types/member';
+import { Member, Photo } from '../../types/member';
 
 
 @Injectable({
@@ -10,22 +9,17 @@ import { Member } from '../../types/member';
 })
 export class MemberService {
   private http = inject(HttpClient);
-  private accountService = inject(AccountService);
   private baseUrl = environment.apiUrl;
 
     getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'members', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + 'members');
   }
 
   getMember(id: string) {
-    return this.http.get<Member>(this.baseUrl + 'members/' + id, this.getHttpOptions());
+    return this.http.get<Member>(this.baseUrl + 'members/' + id);
   }
 
-  private getHttpOptions() {
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + this.accountService.currentUser()?.token
-      })
-    }
+  getMemberPhotos(id: string) {
+    return this.http.get<Photo[]>(this.baseUrl + 'members/' + id + '/photos');
   }
 }
