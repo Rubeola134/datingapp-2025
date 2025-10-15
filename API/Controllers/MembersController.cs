c84ca2ffe98f6b9364b64a15762ef1fcaf9aeaf5
Ruben Garcia <rubendanielgarciat@gmail.com>
2025-10-15 13:23:41 -0400

    added cache, child decorator, candeactivate route guard and keep persistant changes via api.
    Youtube carry

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,4 +1,7 @@
+using System.Security.Claims;
+using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interface;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -31,6 +34,29 @@ namespace API.Controllers
             var photos = await memberRepository.GetPhotosForMemberAsync(id);
 
             return Ok(photos);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+        {
+           var memberId = User.GetMemberId();
+
+            var member = await memberRepository.GetMemberForUpdate(memberId);
+
+            if (member == null) return BadRequest("Oops - no member found with that id");
+
+            member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+            member.Description = memberUpdateDto.Description ?? member.Description;
+            member.City = memberUpdateDto.City ?? member.City;
+            member.Country = memberUpdateDto.Country ?? member.Country;
+
+            member.User.DisplayName = memberUpdateDto.DisplayName ?? member.User.DisplayName;
+
+            memberRepository.Update(member); //optional
+            if (await memberRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update member");
+
         }
     }
 }
